ae1cce79d01f213f1a97ba9306708c28f1c65575
AnatolSalau <salovanatoly5@mail.ru>
2023-12-27 12:30:06 +0300

    Add Separator To String

---
+++ src/strings/add_separator_to_string/AddSeparatorToString.java
@@ -0,0 +1,33 @@
+package strings.add_separator_to_string;
+
+/**
+ * Create string from array with separator
+ */
+public class AddSeparatorToString {
+
+      public static void main(String[] args) {
+            AddSeparatorToString addSeparatorToString = new AddSeparatorToString();
+            addSeparatorToString.test1();
+      }
+      private String getStringWithSeparator(String[] arr, String separator) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < arr.length; i++) {
+                  String curr = arr[i];
+
+                  stringBuilder.append(curr);
+
+                  if (i != arr.length - 1) {
+                        stringBuilder.append(separator);
+                  }
+            }
+            return stringBuilder.toString();
+      }
+      private void test1() {
+            String[] arr = {"One", "Two", "Three", "Four"};
+            String s = getStringWithSeparator(arr, "-");
+            System.out.println(s);
+            System.out.println("Expected result :");
+            System.out.println("One-Two-Three-Four");
+      }
+}
---
+++ src/strings/add_separator_to_string/QuickSorting.md
@@ -0,0 +1,5 @@
+###Add separator to string
+Git link:   
+https://github.com/AnatolSalau/AlgorithmsNotes/tree/master/src/strings/add_separator_to_string
+
+
