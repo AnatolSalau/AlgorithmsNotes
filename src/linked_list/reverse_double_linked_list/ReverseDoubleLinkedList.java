d2282ba8e8d730c0640b6497bfe8e0bc173d1ca7
AnatolSalau <salovanatoly5@mail.ru>
2024-02-17 14:22:58 +0300

    Reverse double linked list by pointers and stack

--- src/linked_list/reverse_double_linked_list/ReverseDoubleLinkedList.java
+++ src/linked_list/reverse_double_linked_list/ReverseDoubleLinkedList.java
@@ -1,5 +1,6 @@
 package linked_list.reverse_double_linked_list;
 
+import java.util.Stack;
 
 public class ReverseDoubleLinkedList {
 
@@ -18,37 +19,6 @@ public class ReverseDoubleLinkedList {
             }
 
       }
-      /*
-      null <-> 5 <-> 4 <-> 3 <-> 2 <-> 1 <-> null
-
-      null <-> 1 <-> 2 <-> 3 <-> 4 <-> 5 <-> null
-
-      i:5
-      null <-> 5 <-> 4
-         swap
-      4 <-> 5 <-> null
-
-
-      i:4
-      5 <-> 4 <-> 3
-         swap
-      3 <-> 4 <-> 5
-
-      i:3
-      4 <-> 3 <-> 2
-         swap
-      2 <-> 3 <-> 4
-
-      i:2
-      3 <-> 2 <-> 1
-         swap
-      1 <-> 2 <-> 3
-
-      i:1
-      2 <-> 1 <-> null
-         swap
-      null <-> 1 <-> 2
-       */
       public static void main(String[] args)
       {
             ReverseDoubleLinkedList reverseList = new ReverseDoubleLinkedList();
@@ -70,41 +40,75 @@ public class ReverseDoubleLinkedList {
 
 
             printList("Original list: ", head1);
-            head1 = reverseList(head1);
+            head1 = reverseListByStack(head1);
             printList("Reversed list: ", head1);
       }
-            /*
-            i:1   prev(null) <-> 1 <-> next(2)  swap  prev(2) <-> 1 <-> next(null)
-            i:2   prev(1) <-> 2 <-> next(3)  swap  prev(3) <-> 2 <-> next(1)
-            */
-      private static Node reverseList(Node head) {
-            Node previous = null;
+      private  Node reverseListByPointers(Node head) {
+            Node prev = null;
             Node curr = head;
-            Node next = null;
 
-            while (curr != null) {
-                  previous = curr.prev;
-                  next = curr.next;
-
-                  //swap prev and next in current
-                  Node temp = curr.prev;
-                  curr.prev = curr.next;
-                  curr.next = temp;
-
-                  if (previous != null) {
-                        previous.prev = curr;
+            // traverse the list
+            while (curr != null)
+            {
+                  // swap `next` and `prev` pointers for the current node
+                  if (prev == null) {
+                        Node temp = curr.prev;
+                        curr.prev = curr.next;
+                        curr.next = temp;
                   }
 
-                  curr = next;
+                  // update the previous node before moving to the next node
+                  prev = curr;
 
-                  System.out.println();
-
+                  // move to the next node in the doubly linked list
+                  curr = curr.prev;
+                  //update links inside curr
+                  if (curr != null) {
+                        curr.prev = curr.next;
+                        curr.next = prev;
+                  }
             }
-            return null;
 
+            // update head pointer to the last node
+            if (prev != null) {
+                  head = prev;
+            }
+
+            return head;
       }
 
-      public static void printList(String msg, Node head)
+      private Node reverseListByStack(Node head) {
+            Stack<Node> stack = new Stack<>();
+            // fill stack
+            Node cur = head;
+            while (cur != null) {
+                  stack.add(cur);
+                  if (cur.next == null) { // add link to las node
+                        head = cur;
+                  }
+                  cur = cur.next;
+
+            }
+            Node previous = null;
+            // change links inside nodes in stack
+            while (!stack.isEmpty()) {
+                  Node node = stack.pop();
+
+                  Node temp = node.prev;
+                  node.prev = node.next;
+                  node.next = temp;
+
+                  if (previous != null) {
+                        previous.next = node;
+                  }
+                  previous = node;
+
+            }
+            return head;
+      }
+
+
+      public  void printList(String msg, Node head)
       {
             System.out.print(msg);
             while (head != null)
