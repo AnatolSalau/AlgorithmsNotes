bddf6ee2705ae51b5f428238e1b39f349e6e86d3
AnatolSalau <salovanatoly5@mail.ru>
2024-02-23 11:35:21 +0300

    Yandex contest task A

---
+++ src/yandex_contest/taks_a/TaskA.java
@@ -0,0 +1,39 @@
+package yandex_contest.taks_a;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskA {
+      /**
+       * Даны две строки строчных латинских символов: строка J и строка S.
+       * Символы, входящие в строку J, — «драгоценности», входящие в строку S — «камни».
+       * Нужно определить, какое количество символов из S одновременно являются «драгоценностями».
+       * Проще говоря, нужно проверить, какое количество символов из S входит в J.
+       */
+      public static void main(String[] args) {
+            String[] jewels = {"a", "b", "c", "d", "e", "f"};
+            String[] stones = { "d", "e", "f"};
+
+            int countOfStonesInJewelBySet = getCountOfStonesInJewelBySet(jewels, stones);
+            System.out.println("countOfStonesInJewelBySet : " + countOfStonesInJewelBySet);
+      }
+
+      /**
+       * O(n + m) - time
+       * O(n) - memory
+       */
+      static int getCountOfStonesInJewelBySet(String[] jewels, String[] stones) {
+            int count = 0;
+            //create set from jewels
+            Set<String> jewelsSet = new HashSet<>();
+            for (int i = 0; i < jewels.length; i++) {
+                  jewelsSet.add(jewels[i]);
+            }
+            //check stones
+            for (int i = 0; i < stones.length; i++) {
+                  if (jewelsSet.contains(stones[i])) count++;
+            }
+            return count;
+      }
+}
