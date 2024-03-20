c5157ce8effb8a0442c046661fd932849c0e2976
AnatolSalau <salovanatoly5@mail.ru>
2024-03-20 12:18:28 +0300

    array of the squares

--- src/two_pointers/sorted_squares_arr/Task34.java
+++ src/two_pointers/sorted_squares_arr/Task34.java
@@ -18,25 +18,69 @@ public class Task34 {
        * Output: [4,9,9,49,121]
        */
       public static void main(String[] args) {
+            test1();
+            test2();
+      }
+
+      private static void test1() {
             System.out.println("Task34");
             int[] nums1 = {-4, -1, 0, 3, 10};
             System.out.println(Arrays.toString(nums1));
             System.out.println("Right answer : ");
             System.out.println("[0, 1, 9, 16, 100]");
             int[] res1 = sortedSquares(nums1);
+            System.out.println("Answer : ");
             System.out.println(Arrays.toString(res1));
             System.out.println();
+      }
+
+      private static void test2() {
             int[] nums2 = {-3,2,3,11};
             System.out.println(Arrays.toString(nums2));
             System.out.println("Right answer : ");
             System.out.println("[4,9,9,121]");
             int[] res2 = sortedSquares(nums2);
+            System.out.println("Answer : ");
             System.out.println(Arrays.toString(res2));
+            System.out.println();
       }
       /*
-             3 1 4 2
-             l     r
+            Arr: -4,-1,0,3,10
+            Res:  0,1,9,16,100
 
+             l         r                    i
+i=4.        -4,-1,0,3,10  res: [0, 0, 0, 0, 0]
+            abs(-4) = 4
+            abs(10) = 10
+            add to resul 10*10 = 100
+            r > l -> decrease r
+
+             l      r                    i
+i=4.        -4,-1,0,3,10  res: [0, 0, 0, 0, 100]
+            abs(-4) = 4
+            abs(3)  = 3
+            add to result 4*4 = 16
+            l > r -> increase l
+
+                l   r                 i
+i=4.        -4,-1,0,3,10  res: [0, 0, 0, 16, 100]
+            abs(-1) = 1
+            abs(3)  = 3
+            add to result 3*3 = 9
+            l < r -> decrease r
+
+                l r                i
+i=4.        -4,-1,0,3,10  res: [0, 0, 9, 16, 100]
+            abs(-1) = 1
+            abs(0)  = 0
+            add to result 1*1 = 1
+            l > r -> increase l
+                  lr            i
+i=4.        -4,-1,0,3,10  res: [0, 1, 9, 16, 100]
+            abs(0)  = 0
+            abs(0)  = 0
+            add to result 0*0 = 0
+            l == r increase l
        */
       static int[] sortedSquares(int[] nums) {
             int[] result = new int[nums.length];
