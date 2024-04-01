fc3ad33c66ecb26f13fe22e2a55ddb3a87a92a2d
AnatolSalau <salovanatoly5@mail.ru>
2024-04-01 16:06:17 +0300

    TreeBranchWithMaxSumRecursive

--- src/yandex_internet/_17_tree_branch_with_max_sum/TreeBranchWithMaxSumRecursive.java
+++ src/yandex_internet/_17_tree_branch_with_max_sum/TreeBranchWithMaxSumRecursive.java
@@ -1,11 +1,15 @@
 package yandex_internet._17_tree_branch_with_max_sum;
 
-import javafx.util.*;
-
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TreeBranchWithMaxSumRecursive {
+      private int curSum = 0;
+      private int maxSum = 0;
+
+      List<Integer> maxPath = new ArrayList<>();
+      List<Integer> currPath = new ArrayList<>();
+
       public static class TreeNode {
             int val;
             TreeNode left;
@@ -29,6 +33,7 @@ public class TreeBranchWithMaxSumRecursive {
                right answer is 1, 9, 5, 7 O(n)
        */
       private static void test1() {
+            TreeBranchWithMaxSumRecursive branchWithMaxSumRecursive = new TreeBranchWithMaxSumRecursive();
             TreeNode _7Left3 = new TreeNode(7, null, null);
             TreeNode _9Left3 = new TreeNode(9, null, null);
 
@@ -42,15 +47,35 @@ public class TreeBranchWithMaxSumRecursive {
             TreeNode _9Right1 = new TreeNode(9, _5Left2, _2Right2);
 
             TreeNode root0 = new TreeNode(1,_2Left1, _9Right1);
-            List<Integer> resultWithSum = findNumbersNodesInBranchWithMaxSumRecursive(root0);
-            System.out.println("Result :" + resultWithSum);
+            branchWithMaxSumRecursive.findNumbersNodesInBranchWithMaxSumRecursive(root0);
+            System.out.println("Result :" + branchWithMaxSumRecursive.maxPath);
             System.out.println("Expect : [1, 9, 5, 7]");
       }
+      /*
+            Explanation:
+            1. Go to bottom throught DFS
+            2. Create variables at class level
+                  currSum, maxSum
+                  currPath, maxPath
+            3. In direct path DFS
+                  add curr value to currSum
+                  add curr value to currPath
+            4. Run recursive DFS for left half
+            5. Run recursive DFS for left half
+            6. If curr sum more max sum
+                  add to max path curr path
+                  add to max sum curr sum
+            7. remove from curr sum -> curr value
+            8. remove from curr path -> last letter in list
 
-      private static List<Integer> findNumbersNodesInBranchWithMaxSumRecursive(TreeNode root) {
+       */
+      private void findNumbersNodesInBranchWithMaxSumRecursive(TreeNode node) {
 
-            TreeNode leftNode = root.left;
-            TreeNode rightNode = root.right;
+            TreeNode leftNode = node.left;
+            TreeNode rightNode = node.right;
+
+            curSum += node.val;
+            currPath.add(node.val);
 
             if (leftNode != null) {
                   findNumbersNodesInBranchWithMaxSumRecursive(leftNode);
@@ -58,9 +83,13 @@ public class TreeBranchWithMaxSumRecursive {
             if (rightNode != null) {
                   findNumbersNodesInBranchWithMaxSumRecursive(rightNode);
             }
+            if (curSum > maxSum) {
+                  maxPath = List.copyOf(currPath);
+                  maxSum = curSum;
+            }
+            curSum -= node.val;
+            currPath.remove(currPath.size() - 1);
 
-
-            return null;
       }
 
       public static void main(String[] args) {
