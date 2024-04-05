5512a2419ba9d7aac32aabc764d4c8c94c788f15
AnatolSalau <salovanatoly5@mail.ru>
2024-04-05 14:45:14 +0300

    TaskScheduler

--- src/alex_adam/_2_task_scheduler/TaskScheduler.java
+++ src/alex_adam/_2_task_scheduler/TaskScheduler.java
@@ -8,43 +8,44 @@ import java.util.stream.Stream;
 
 /**
  * https://leetcode.com/problems/task-scheduler/description/
- You are given an array of CPU tasks, each represented by letters A to Z,
- and a cooling time, n. Each cycle or interval allows the completion of one task.
- Tasks can be completed in any order, but there's a constraint:
- identical tasks must be separated by at least n intervals due to cooling time.
-
- !!! Return the minimum number of intervals required to complete all tasks.
-
- Example 1:
- Input: tasks = ["A","A","A","B","B","B"], n = 2
- Output: 8
- Explanation: A possible sequence is: A -> B -> idle -> A -> B -> idle -> A -> B.
- After completing task A, you must wait two cycles before doing A again.
- The same applies to task B.
- In the 3rd interval, neither A nor B can be done, so you idle. B
- y the 4th cycle, you can do A again as 2 intervals have passed.
-
- Example 2:
- Input: tasks = ["A","C","A","B","D","B"], n = 1
- Output: 6
- Explanation: A possible sequence is: A -> B -> C -> D -> A -> B.
- With a cooling interval of 1, you can repeat a task after just one other task.
-
- Example 3:
- Input: tasks = ["A","A","A", "B","B","B"], n = 3
- Output: 10
- Explanation: A possible sequence is: A -> B -> idle -> idle -> A -> B -> idle -> idle -> A -> B.
- There are only two types of tasks, A and B, which need to be separated by 3 intervals.
- This leads to idling twice between repetitions of these tasks.
+ * You are given an array of CPU tasks, each represented by letters A to Z,
+ * and a cooling time, n. Each cycle or interval allows the completion of one task.
+ * Tasks can be completed in any order, but there's a constraint:
+ * identical tasks must be separated by at least n intervals due to cooling time.
+ * <p>
+ * !!! Return the minimum number of intervals required to complete all tasks.
+ * <p>
+ * Example 1:
+ * Input: tasks = ["A","A","A","B","B","B"], n = 2
+ * Output: 8
+ * Explanation: A possible sequence is: A -> B -> idle -> A -> B -> idle -> A -> B.
+ * After completing task A, you must wait two cycles before doing A again.
+ * The same applies to task B.
+ * In the 3rd interval, neither A nor B can be done, so you idle. B
+ * y the 4th cycle, you can do A again as 2 intervals have passed.
+ * <p>
+ * Example 2:
+ * Input: tasks = ["A","C","A","B","D","B"], n = 1
+ * Output: 6
+ * Explanation: A possible sequence is: A -> B -> C -> D -> A -> B.
+ * With a cooling interval of 1, you can repeat a task after just one other task.
+ * <p>
+ * Example 3:
+ * Input: tasks = ["A","A","A", "B","B","B"], n = 3
+ * Output: 10
+ * Explanation: A possible sequence is: A -> B -> idle -> idle -> A -> B -> idle -> idle -> A -> B.
+ * There are only two types of tasks, A and B, which need to be separated by 3 intervals.
+ * This leads to idling twice between repetitions of these tasks.
  */
 public class TaskScheduler {
       public static void main(String[] args) {
             TaskScheduler taskScheduler = new TaskScheduler();
-            //taskScheduler.test1();
+            taskScheduler.test1();
             //taskScheduler.test2();
-            taskScheduler.test3();
+            //taskScheduler.test3();
             //taskScheduler.test4();
       }
+
       public int leastInterval(char[] tasks, int n) {
             Map<Character, Integer> freqMap = IntStream
                   .range(0, tasks.length)
@@ -55,7 +56,7 @@ public class TaskScheduler {
                   ));
             List<Map.Entry<Character, Integer>> entries = new ArrayList<>(freqMap.entrySet());
 
-            Collections.sort(entries,  (entry1, entry2) -> entry2.getValue() - entry1.getValue());
+            Collections.sort(entries, (entry1, entry2) -> entry2.getValue() - entry1.getValue());
             //Collections.sort(entries, (entry1, entry2) -> entry1.getValue() - entry2.getValue());
 
             List<Character> result = new LinkedList<>();
@@ -71,23 +72,17 @@ public class TaskScheduler {
 
                         if (newCount == 0) {
                               entries.remove(i);
-                        }
-                        else {
+                        } else {
                               curr.setValue(newCount);
                         }
                         steps++;
                   }
-                  Character nextCharacter = null;
-                  if(!entries.isEmpty()) {
-                        nextCharacter = entries.get(0).getKey();
 
-                              if (steps != 0 && (nextCharacter != result.get(result.size() - 1)) ) {
-                                    while (steps <= n) {
-                                          result.add('_');
-                                          steps++;
-                                    }
-                              }
-
+                  if (!entries.isEmpty()) {
+                        while (steps <= n  && steps <= entries.size() ) {
+                              result.add('_');
+                              steps++;
+                        }
                   }
 
             }
@@ -96,8 +91,8 @@ public class TaskScheduler {
             return result.size();
       }
 
-      public  void test1() {
-            char[] tasks = {'A','A','A', 'B','B','B'};
+      public void test1() {
+            char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B' };
             int n = 2;
             int expect = 8;
             int result = this.leastInterval(tasks, n);
@@ -107,8 +102,8 @@ public class TaskScheduler {
             System.out.println("Expected : " + expect);
       }
 
-      public  void test2() {
-            char[] tasks = {'A','C','A', 'B','D','B'};
+      public void test2() {
+            char[] tasks = {'A', 'C', 'A', 'B', 'D', 'B' };
             int n = 1;
             int expect = 6;
             int result = this.leastInterval(tasks, n);
@@ -118,8 +113,8 @@ public class TaskScheduler {
             System.out.println("Expected : " + expect);
       }
 
-      public  void test3() {
-            char[] tasks = {'A','A','A', 'B','B','B'};
+      public void test3() {
+            char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B' };
             int n = 3;
             int expect = 10;
             int result = this.leastInterval(tasks, n);
@@ -129,8 +124,8 @@ public class TaskScheduler {
             System.out.println("Expected : " + expect);
       }
 
-      public  void test4() {
-            char[] tasks = {'A','B','A'};
+      public void test4() {
+            char[] tasks = {'A', 'B', 'A' };
             int n = 2;
             int expect = 4;
             int result = this.leastInterval(tasks, n);
