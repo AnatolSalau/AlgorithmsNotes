793d3f22d4ae2cf6a1624c124fd3edc98a15a94c
AnatolSalau <salovanatoly5@mail.ru>
2024-04-12 15:13:15 +0300

    Baeldung Reduce

--- src/stream_api/grouping_examples/GroupingExamplesBaeldung.java
+++ src/stream_api/grouping_examples/GroupingExamplesBaeldung.java
@@ -1,7 +1,51 @@
 package stream_api.grouping_examples;
 
+
+import javafx.util.Pair;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
 /**
  * https://www.baeldung.com/java-groupingby-collector
  */
 public class GroupingExamplesBaeldung {
+
+      static enum BlogPostType {NEWS, REVIEW, GUIDE}
+
+      static record BlogPost(String title, String author, BlogPostType type, int likes) {}
+
+
+      public static void main(String[] args) {
+
+            List<BlogPost> posts = Arrays.asList(
+                  new BlogPost("Title1", "Autthor1", BlogPostType.NEWS, 1),
+                  new BlogPost("Title2", "Autthor2", BlogPostType.REVIEW, 2),
+                  new BlogPost("Title3", "Autthor3", BlogPostType.GUIDE, 3),
+                  new BlogPost("Title4", "Autthor4", BlogPostType.NEWS, 4)
+            );
+
+            //groupByBlogPostType(posts);
+            groupByPairTypeName(posts);
+      }
+
+      public static void groupByBlogPostType(List<BlogPost> posts) {
+            Map<BlogPostType, List<BlogPost>> collect = posts.stream()
+                  .collect(Collectors.groupingBy(BlogPost::type));
+            System.out.println(collect);
+      }
+
+      public static void groupByPairTypeName(List<BlogPost> posts) {
+            Map<Pair<BlogPostType, String>, List<BlogPost>> collect = posts.stream()
+                  .collect(Collectors.groupingBy(
+                        post -> new Pair(post.type, post.author)
+                  ));
+            System.out.println(collect);
+      }
+
+
+
 }
