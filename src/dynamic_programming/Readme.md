159e25145e04fc2139ea5d926ddb48901eba97a7
AnatolSalau <salovanatoly5@mail.ru>
2024-08-23 12:44:56 +0300

    MapMergerTest

--- src/dynamic_programming/Readme.md
+++ src/dynamic_programming/Readme.md
@@ -1,2 +1,3 @@
 
-       https://www.youtube.com/watch?v=zqYShUzw36g&list=PLUfHxBkkFMScqPOn8J0aHvd48wykQNcWS&index=2&ab_channel=IgorKleinerPh.D.
+        https://www.youtube.com/watch?v=zqYShUzw36g&list=PLUfHxBkkFMScqPOn8J0aHvd48wykQNcWS&index=2&ab_channel=IgorKleinerPh.D.
+        https://www.youtube.com/watch?v=AYB-mSsBIXY&list=PLUfHxBkkFMScK6mOOWp5s6LgbzmtfwmYQ&index=6
---
+++ src/dynamic_programming/_1_ariphmetic_progression/AriphmeticProgression.java
@@ -0,0 +1,23 @@
+package dynamic_programming._1_ariphmetic_progression;
+
+public class AriphmeticProgression {
+      /**
+       * Find sum numbers from n to m by dynamic programming
+       * @param args
+       */
+      public static void main(String[] args) {
+            AriphmeticProgression ariphmeticProgression = new AriphmeticProgression();
+            System.out.println("Sum from 1 to 5 (1,2,3,4,5) = 15");
+            System.out.println("Answer: " + ariphmeticProgression.findSumFromNToM(1,5));
+            System.out.println();
+
+      }
+
+      public int findSumFromNToM(int n, int m) {
+            if (n <= m) {
+                  int recursiveResult = findSumFromNToM(n+1, m);//direct recursion way
+                  return n + recursiveResult;//reverse recursion way
+            }
+            return 0;
+      }
+}
