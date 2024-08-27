e3d623b5e2b94c7e1283554cb68edd7b5e791bf0
AnatolSalau <salovanatoly5@mail.ru>
2024-08-27 14:10:20 +0300

    RestOfChange Dynamic programming

--- src/dynamic_programming/rest_of_change/RestOfChange.java
+++ src/dynamic_programming/rest_of_change/RestOfChange.java
@@ -1,8 +1,9 @@
 package dynamic_programming.rest_of_change;
 
 /**
- * <a href="https://www.youtube.com/watch?v=AYB-mSsBIXY&list=PLUfHxBkkFMScK6mOOWp5s6LgbzmtfwmYQ&index=6">...</a>
- * <a href="https://www.youtube.com/watch?v=-apNZkl_yPI&list=PLUfHxBkkFMScK6mOOWp5s6LgbzmtfwmYQ&index=7">...</a>
+ * <a href="https://www.youtube.com/watch?v=-apNZkl_yPI&list=PLUfHxBkkFMScK6mOOWp5s6LgbzmtfwmYQ&index=7">...</a> Theory
+ *
+ * <a href="https://www.youtube.com/watch?v=NNcN5X1wsaw">...</a> Java
  */
 public class RestOfChange {
 
