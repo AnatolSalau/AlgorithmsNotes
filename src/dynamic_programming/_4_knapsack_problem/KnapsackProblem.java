dd39b8a734b06e9d6ac79133c547d24689f56946
AnatolSalau <salovanatoly5@mail.ru>
2024-09-11 15:22:04 +0300

    Rest of change by dynamic programming

--- src/dynamic_programming/_4_knapsack_problem/KnapsackProblem.java
+++ src/dynamic_programming/_4_knapsack_problem/KnapsackProblem.java
@@ -2,6 +2,7 @@ package dynamic_programming._4_knapsack_problem;
 
 /**
  * <a href="https://www.youtube.com/watch?v=fqvwVE0Ds94&ab_channel=NikhilLohia">...</a>
+ * <a href="https://www.youtube.com/watch?v=wcjqBf2qRe0&ab_channel=SergeyArkhipovJavaTutorials">...</a>
  */
 public class KnapsackProblem {
 }
