9951c302c2448ff51a2de743047de66abe93f457
AnatolSalau <salovanatoly5@mail.ru>
2024-09-17 17:57:57 +0300

    KnapsackProblem2.java

---
+++ src/dynamic_programming/_4_knapsack_problem/KnapsackProblem2.java
@@ -0,0 +1,10 @@
+package dynamic_programming._4_knapsack_problem;
+
+/*
+    https://neerc.ifmo.ru/wiki/index.php?title=%D0%97%D0%B0%D0%B4%D0%B0%D1%87%D0%B0_%D0%BE_%D1%80%D1%8E%D0%BA%D0%B7%D0%B0%D0%BA%D0%B5
+ */
+public class KnapsackProblem2 {
+    public static void main(String[] args) {
+
+    }
+}
