a283a1356cc0bd0ef9a47fb114aad8f03f24f832
AnatolSalau <salovanatoly5@mail.ru>
2024-09-18 18:15:51 +0300

    KnapsackProblem2.java

--- src/dynamic_programming/_4_knapsack_problem/KnapsackProblem2.java
+++ src/dynamic_programming/_4_knapsack_problem/KnapsackProblem2.java
@@ -5,6 +5,48 @@ package dynamic_programming._4_knapsack_problem;
  */
 public class KnapsackProblem2 {
     public static void main(String[] args) {
+        test1();
+    }
 
+    public static void test1() {
+        int capacity = 13;
+        int qty = 5;
+        int[] capacities = {3, 4, 5, 8, 9};
+        int[] prices = {1, 6, 4, 7, 6};
+
+        int maxPrice = getMaxPrice(capacity, qty, capacities, prices);
+        System.out.println("Max price");
+        System.out.println(maxPrice);
+        System.out.println("Expected result = 13");
+    }
+
+    private static int getMaxPrice(int capacityOfKnapsack, int qty, int[] capacities, int[] prices) {
+        int[][] memorization = new int[qty + 1][capacityOfKnapsack + 1];
+        int startColumn = capacities[0]; // get first capacity
+
+        for (int i = 1; i < qty + 1; i++) {
+            int[] currRow = memorization[i];
+            for (int j = startColumn; j < capacityOfKnapsack; j++) {
+                int colMaxCapacity = j;
+                int thingCapacity = getThingCapacity(capacities, i - 1);
+                int thingPrice = getThingPrice(prices, i - 1);
+                int remainder = thingCapacity - colMaxCapacity;
+                int maxCapacityFromColumn = getMaxCapacityFromColumn(memorization, remainder, i);
+                System.out.println();
+            }
+        }
+        return 0;
+    }
+
+    private static int getMaxCapacityFromColumn(int[][] memorization, int remainder, int currRowIndex) {
+        return memorization[currRowIndex - 1][remainder];
+    }
+
+    private static int getThingPrice(int[] prices, int thingIndex) {
+        return prices[thingIndex];
+    }
+
+    private static int getThingCapacity(int[] capacities, int thingIndex) {
+        return capacities[thingIndex];
     }
 }
